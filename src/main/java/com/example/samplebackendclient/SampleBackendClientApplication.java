9d2cb36cc7dd880be1801a53fe10bdf4162d9008
kuritayu <summergirlna@gmail.com>
2026-08-22 13:16:33 +0900

    Initialize `sample-backend-client` module with Maven project structure, Spring Boot setup, and starter dependencies.

---
+++ src/main/java/com/example/samplebackendclient/SampleBackendClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplebackendclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBackendClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleBackendClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplebackendclient/SampleBackendClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplebackendclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBackendClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
